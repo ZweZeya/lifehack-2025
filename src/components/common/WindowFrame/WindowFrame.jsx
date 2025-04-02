c66b780ed2828a719b1ed2ff89ac168942a653f2
zwe <zwezeya02@gmail.com>
2025-04-02 22:30:20 +0800

    add window terminal

---
+++ src/components/common/WindowFrame/WindowFrame.jsx
@@ -0,0 +1,33 @@
+import "./WindowFrame.scss"
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
+const WindowFrame = (props) => {
+    const glitches = [
+        "",
+        "glitch1",
+        "glitch2",
+        "glitch3",
+        "glitch4",
+    ]
+    const [glitchType, setGlitchType] = useState(0);
+
+    const handleClick = () => {
+        setGlitchType((glitchType + 1) % 4)
+    }
+
+    return (
+        <div className={`container ${glitches[glitchType + 1]}`}>
+            <div className="top-bar">
+                <div className="close-btn" onClick={handleClick}>
+                    <AiOutlineClose className="close-icon" />
+                </div>
+            </div>
+            <div className="content-container">
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default WindowFrame;
