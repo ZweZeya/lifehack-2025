ec422d7a2049cee3cd7a550822ce7413073848fb
leslie yip <leslieyip02@gmail.com>
2025-04-03 03:34:26 +0800

    feat: basic vaporwave background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import AboutWindowFrame from './components/About/AboutWindowFrame/AboutWindowFrame'
+import Background from './components/Background/Background'
 
 function App() {
     return (
         <>
+            <Background />
+
             <div>
                 <AboutWindowFrame />
             </div>
