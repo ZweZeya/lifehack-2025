dbb5698cf7eb0013182b9ce528864eda5b8bdcd9
zwe <zwezeya02@gmail.com>
2025-04-04 00:55:04 +0800

    wip tree

--- src/components/ProblemStatements/ProblemStatementsTree/ProblemStatementsTree.jsx
+++ src/components/ProblemStatements/ProblemStatementsTree/ProblemStatementsTree.jsx
@@ -1,4 +1,5 @@
 import "./ProblemStatementsTree.scss"
+import { useState, useEffect } from "react"
 
 const ProblemStatementsTree = () => {
     const data = {
@@ -10,42 +11,122 @@ const ProblemStatementsTree = () => {
                 key: "Theme 1",
                 label: "Theme 1: Health and Wellbeing",
                 content: "Harness data‑driven technology to tackle inefficiencies in healthcare that improve community health outcomes.",
-                children: []
+                children: [
+                    {
+                        key: "Theme 1.1",
+                        label: "Placeholder label 1.1",
+                        content: "Placeholder content 1.1",
+                        children: []
+                    },
+                    {
+                        key: "Theme 1.2",
+                        label: "Placeholder label 1.2",
+                        content: "Placeholder content 1.2",
+                        children: []
+                    },
+                    {
+                        key: "Theme 1.3",
+                        label: "Placeholder label 1.3",
+                        content: "Placeholder content 1.3",
+                        children: []
+                    }
+                ]
             },
             {
                 key: "Theme 2",
                 label: "Theme 2: Knowledge and Collaboration",
                 content: "Design digital solutions that personalise learning and break barriers so everyone can access knowledge anytime, anywhere.",
-                children: []
+                children: [
+                    {
+                        key: "Theme 2.1",
+                        label: "Placeholder label 2.1",
+                        content: "Placeholder content 2.1",
+                        expandedStates: [],
+                        children: []
+                    },
+                    {
+                        key: "Theme 2.2",
+                        label: "Placeholder label 2.2",
+                        content: "Placeholder content 2.2",
+                        expandedStates: [],
+                        children: []
+                    },
+                    {
+                        key: "Theme 2.3",
+                        label: "Placeholder label 2.3",
+                        content: "Placeholder content 2.3",
+                        expandedStates: [],
+                        children: []
+                    }
+                ]
             },
             {
                 key: "Theme 3",
                 label: "Theme 3: Sustainability & Consumption",
                 content: "Create tools that empower consumers with transparent insights, and inspire meaningful behaviour change toward a greener future.",
-                children: []
+                children: [
+                    {
+                        key: "Theme 3.1",
+                        label: "Placeholder label 3.1",
+                        content: "Placeholder content 3.1",
+                        children: []
+                    },
+                    {
+                        key: "Theme 3.2",
+                        label: "Placeholder label 3.2",
+                        content: "Placeholder content 3.2",
+                        children: []
+                    },
+                    {
+                        key: "Theme 3.3",
+                        label: "Placeholder label 3.3",
+                        content: "Placeholder content 3.3",
+                        children: []
+                    }
+                ]
             }
         ]
     }
 
     return (
         <div>
-            <TreeNode data={data} />
+            <TreeNode data={data} isExpanded={true} />
         </div>
     )
 }
 
 const TreeNode = (props) => {
-    const data = props.data
+    const [expandedStates, setExpandedStates] = useState([]);
+
+    const { data, isExpanded } = props
+
+    useEffect(() => {
+        setExpandedStates(data.children.map(() => false))
+    }, [])
+
+    const handleClick = (index) => {
+        if (expandedStates[index]) return
+        setExpandedStates(curr => curr.map((v, i) => i == index ? true : false))
+    }
+
     return (
-        <>
-            <div className="node-container">
+        <div>
+            <div className="node-container" onClick={() => {}}>
                 <p>{data.label}</p>
-                
+                <p>{data.content}</p>
             </div>
-            <div className="child-nodes-container">
-                {data.children.map(e => <TreeNode key={e.key} data={e} />)}
-            </div>
-        </>
+            { isExpanded && expandedStates.length > 0 &&
+              <div>
+                <ul className="child-nodes-container">
+                    { data.children.map((v, i) => 
+                        <li key={v.key} onClick={() => handleClick(i)}>
+                            <TreeNode data={v} isExpanded={expandedStates[i]} />
+                        </li>) 
+                    }
+                </ul>
+              </div>
+            }
+        </div>
     )
 }
 
