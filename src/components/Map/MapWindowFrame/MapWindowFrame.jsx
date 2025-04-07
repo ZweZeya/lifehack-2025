dfe0d1d8c74a644be7688b4a16ad398baf375805
zwe <zwezeya02@gmail.com>
2025-04-07 23:35:19 +0800

    fix map marker

--- src/components/Map/MapWindowFrame/MapWindowFrame.jsx
+++ src/components/Map/MapWindowFrame/MapWindowFrame.jsx
@@ -1,10 +1,19 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
 import WindowFrame from "../../common/WindowFrame/WindowFrame"
 import "./MapWindowFrame.scss"
-import Header from "../../common/Header/Header";
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const MapWindowFrame = ({className}) => {
     const position = [1.294872, 103.7737049];
